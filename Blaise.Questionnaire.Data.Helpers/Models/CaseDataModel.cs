db93ae9f12e19a6fcdfd31563c79f9422da7d120
jamie kerr <jamiestephenkerr@gmail.com>
2023-10-31 15:23:29 +0000

    Updated initialise fields in case data model

--- Blaise.Questionnaire.Data.Helpers/Models/CaseDataModel.cs
+++ Blaise.Questionnaire.Data.Helpers/Models/CaseDataModel.cs
@@ -9,7 +9,7 @@ namespace Blaise.Questionnaire.Data.Helpers.Models
         public CaseDataModel(int primaryKey, Dictionary<string, string> sampleDataFields = null)
         {
             PrimaryKey = primaryKey.ToString();
-            DataFields = InitialiseCaseDataFields(sampleDataFields);
+            DataFields = sampleDataFields == null ? InitialiseCaseDataFields() : InitialiseCaseDataFieldsFromSample(sampleDataFields);
         }
 
         public CaseModel ToCaseModel()
@@ -17,7 +17,70 @@ namespace Blaise.Questionnaire.Data.Helpers.Models
            return new CaseModel(PrimaryKey, DataFields);
         }
 
-        public Dictionary<string, string> InitialiseCaseDataFields(Dictionary<string, string> dataFields)
+        public Dictionary<string, string> InitialiseCaseDataFields()
+        {
+            var dataFields = new Dictionary<string, string>
+            {
+                ["qid.serial_number"] = PrimaryKey,
+                ["QDataBag.uac1"] = Uac1,
+                ["QDataBag.uac2"] = Uac2,
+                ["QDataBag.uac3"] = Uac3,
+
+                ["qdatabag.tla"] = "tla",
+                ["qdatabag.wave"] = "1",
+                ["qdatabag.telno"] = "07000000000",
+                ["qdatabag.telno2"] = "07000000000",
+                ["qdatabag.telnoappt"] = "07000000000",
+                ["qdatabag.prem1"] = "prem1",
+                ["qdatabag.prem2"] = "prem2",
+                ["qdatabag.prem3"] = "prem3",
+                ["qdatabag.district"] = "district",
+                ["qdatabag.posttown"] = "posttown",
+                ["qdatabag.postcode"] = "np10 8xg",
+                ["qdatabag.uprn_latitude"] = "51.566",
+                ["qdatabag.uprn_longitude"] = "-3.026",
+                ["qdatabag.samptitle"] = "samptitle",
+                ["qdatabag.sampfname"] = "sampfname",
+                ["qdatabag.sampsname"] = "sampsname",
+                ["qdatabag.name"] = "name",
+                ["qdatabag.fieldcase"] = "y",
+                ["qdatabag.priority"] = "1",
+                ["qdatabag.fieldteam"] = "fieldteam",
+                ["qdatabag.fieldregion"] = "region 1",
+                ["qdatabag.wavecomdte"] = "01-01-3000",
+                ["qsample.orgname"] = "orgname",
+                ["qsample.address1"] = "address1",
+                ["qsample.address2"] = "address2",
+                ["qsample.locality"] = "locality",
+                ["qsample.townname"] = "townname",
+                ["qsample.postcode"] = "postcode",
+                ["qsample.telno"] = "07000000000",
+                ["qsample.telno2"] = "07000000000",
+                ["qhadmin.hout"] = "110",
+                ["hout"] = "110",
+                ["qhadmin.interviewer[1]"] = "interviewer1",
+                ["dmhsize"] = "2",
+                ["dmname[1]"] = "dmname1",
+                ["dmdteofbth[1]"] = "01-01-2000",
+                ["dmname[2]"] = "dmname2",
+                ["dmdteofbth[2]"] = "01-01-2000",
+                ["qhousehold.qhhold.person[1].title"] = "title",
+                ["qhousehold.qhhold.person[1].fstnme"] = "fstnme",
+                ["qhousehold.qhhold.person[1].surnme"] = "surnme",
+                ["qrotate.rdmktnind"] = "",
+                ["qrotate.rhout"] = "",
+                ["catimana.caticall.regscalls[1].whomade"] = "whomade",
+                ["catimana.caticall.regscalls[5].dialresult"] = "1",
+                ["catimana.caticall.regscalls[1].whomade"] = "whomade",
+                ["catimana.caticall.regscalls[5].dialresult"] = "1",
+                ["datamodelname"] = "datamodelname",
+                ["offlinecapi.towhom"] = "towhom",
+            };
+
+            return dataFields;
+        }
+
+        public Dictionary<string, string> InitialiseCaseDataFieldsFromSample(Dictionary<string, string> dataFields)
         {
             if (dataFields is null)
             {
@@ -25,67 +88,12 @@ namespace Blaise.Questionnaire.Data.Helpers.Models
             }
 
             dataFields["qid.serial_number"] = PrimaryKey;
-            dataFields["qdatabag.tla"] = "tla";
-            dataFields["qdatabag.wave"] = "1";
-            dataFields["qdatabag.telno"] = "07000000000";
-            dataFields["qdatabag.telno2"] = "07000000000";
-            dataFields["qdatabag.telnoappt"] = "07000000000";
-            dataFields["qdatabag.prem1"] = "prem1";
-            dataFields["qdatabag.prem2"] = "prem2";
-            dataFields["qdatabag.prem3"] = "prem3";
-            dataFields["qdatabag.district"] = "district";
-            dataFields["qdatabag.posttown"] = "posttown";
-            dataFields["qdatabag.postcode"] = "np10 8xg";
-            dataFields["qdatabag.uprn_latitude"] = "51.566";
-            dataFields["qdatabag.uprn_longitude"] = "-3.026";
-            dataFields["qdatabag.samptitle"] = "samptitle";
-            dataFields["qdatabag.sampfname"] = "sampfname";
-            dataFields["qdatabag.sampsname"] = "sampsname";
-            dataFields["qdatabag.name"] = "name";
-            dataFields["qdatabag.fieldcase"] = "y";
-            dataFields["qdatabag.priority"] = "1";
-            dataFields["qdatabag.fieldteam"] = "fieldteam";
-            dataFields["qdatabag.fieldregion"] = "region 1";
-            dataFields["qdatabag.wavecomdte"] = "01-01-3000";
-            dataFields["qsample.orgname"] = "orgname";
-            dataFields["qsample.address1"] = "address1";
-            dataFields["qsample.address2"] = "address2";
-            dataFields["qsample.locality"] = "locality";
-            dataFields["qsample.townname"] = "townname";
-            dataFields["qsample.postcode"] = "postcode";
-            dataFields["qsample.telno"] = "07000000000";
-            dataFields["qsample.telno2"] = "07000000000";
-            dataFields["qhadmin.hout"] = "110";
-            dataFields["hout"] = "110";
-            dataFields["qhadmin.interviewer[1]"] = "interviewer1";
-            dataFields["dmhsize"] = "2";
-            dataFields["dmname[1]"] = "dmname1";
-            dataFields["dmdteofbth[1]"] = "01-01-2000";
-            dataFields["dmname[2]"] = "dmname2";
-            dataFields["dmdteofbth[2]"] = "01-01-2000";
-            dataFields["qhousehold.qhhold.person[1].title"] = "title";
-            dataFields["qhousehold.qhhold.person[1].fstnme"] = "fstnme";
-            dataFields["qhousehold.qhhold.person[1].surnme"] = "surnme";
-            dataFields["qrotate.rdmktnind"] = "";
-            dataFields["qrotate.rhout"] = "";
-            dataFields["catimana.caticall.regscalls[1].whomade"] = "whomade";
-            dataFields["catimana.caticall.regscalls[5].dialresult"] = "1";
-            dataFields["catimana.caticall.regscalls[1].whomade"] = "whomade";
-            dataFields["catimana.caticall.regscalls[5].dialresult"] = "1";
-            dataFields["datamodelname"] = "datamodelname";
-            dataFields["offlinecapi.towhom"] = "towhom";
+            dataFields["QDataBag.uac1"] = Uac1;
+            dataFields["QDataBag.uac2"] = Uac2;
+            dataFields["QDataBag.uac3"] = Uac3;
+
 
             return dataFields;
-        }
-
-        private static void SetDefaultValueIfNull(IDictionary<string, string> dataFields, string fieldName, string defaultValue)
-        {
-            if (dataFields.ContainsKey(fieldName) && !string.IsNullOrWhiteSpace(dataFields[fieldName]))
-            {
-                return;
-            }
-
-            dataFields[fieldName] = defaultValue;
         }
 
         public string PrimaryKey { get; set; }
